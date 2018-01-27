ca5af9978cb5b86231766e06d0fd3a03e2c94de6
PeachTreeOath <peachtreeoath@gmail.com>
2018-01-27 00:57:32 -0700

    Transferring stuff to work at home

---
+++ Assets/Resources/Scripts/StickyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyBehavior : MonoBehaviour {
+
+
+    public Collider2D leftCollider;
+    public Collider2D rightCollider;
+
+    public void ActivateLeftSticky()
+    {
+        ContactFilter2D filter = new ContactFilter2D();
+        Collider2D[] results = new Collider2D[10];
+        leftCollider.OverlapCollider(filter, results);
+    }
+
+    public void ActivateRightSticky()
+    {
+        o
+    }
+}
