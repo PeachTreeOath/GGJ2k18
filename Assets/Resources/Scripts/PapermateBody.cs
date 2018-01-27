ac3b7c076c139d57f885aba3cdea68786343893a
PeachTreeOath <peachtreeoath@gmail.com>
2018-01-27 02:25:02 -0700

    Added attaching capability

--- Assets/Resources/Scripts/PapermateBody.cs
+++ Assets/Resources/Scripts/PapermateBody.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 /// PapermateBody behavior is responsible for building out the papermate body.
 /// </summary>
 [RequireComponent(typeof(LineRenderer))]
-public class PapermateBody : MonoBehaviour {
+public class PapermateBody : MonoBehaviour
+{
 
     public int jointCount = 10;
     public int unitLength = 4;
@@ -16,14 +17,16 @@ public class PapermateBody : MonoBehaviour {
     private float _radius;
     private LineRenderer _lineRenderer;
     private List<GameObject> _joints;
+    private CircleCollider2D leftCollider;
+    private CircleCollider2D rightCollider;
 
-	// Use this for initialization
-	private void Start ()
+    // Use this for initialization
+    private void Start()
     {
         _radius = width / 2f;
         _lineRenderer = GetComponent<LineRenderer>();
         InitializeBody();
-	}
+    }
 
     // used to build out the physics body and joints of papermate
     private void InitializeBody()
@@ -34,8 +37,10 @@ public class PapermateBody : MonoBehaviour {
         for (int i = 0; i < jointCount; i++)
         {
             GameObject joint = new GameObject("joint_" + i);
-            joint.transform.localPosition = new Vector3(transform.position.x , transform.position.y + (segLen * i), transform.position.z);
-            
+            joint.transform.localPosition = new Vector3(transform.position.x, transform.position.y + (segLen * i), transform.position.z);
+            joint.layer = LayerMask.NameToLayer("Nonattachable");
+            joint.transform.SetParent(transform);
+
             CircleCollider2D circleCollider = joint.AddComponent<CircleCollider2D>();
             circleCollider.radius = _radius;
 
@@ -55,6 +60,20 @@ public class PapermateBody : MonoBehaviour {
 
             _joints.Add(joint);
             prevBody = body;
+
+            // Add colliders on the ends for latching onto objects
+            if (i == 0)
+            {
+                leftCollider = joint.AddComponent<CircleCollider2D>();
+                leftCollider.radius = _radius * 1.5f;
+                leftCollider.isTrigger = true;
+            }
+            if (i == jointCount - 1)
+            {
+                rightCollider = joint.AddComponent<CircleCollider2D>();
+                rightCollider.radius = _radius * 1.5f;
+                rightCollider.isTrigger = true;
+            }
         }
 
         _lineRenderer.useWorldSpace = true;
@@ -72,9 +91,59 @@ public class PapermateBody : MonoBehaviour {
         float v2 = Input.GetAxis("J_RightStickY");
         _joints.Last().GetComponent<Rigidbody2D>().AddForce(new Vector2(h2 * power, v2 * power));
 
+        if (Input.GetButtonDown("J_LeftStickPress"))
+            LockLeftJoint();
+        else if (Input.GetButtonUp("J_LeftStickPress"))
+            UnlockLeftJoint();
+
+        if (Input.GetButtonDown("J_RightStickPress"))
+            LockRightJoint();
+        else if (Input.GetButtonUp("J_RightStickPress"))
+            UnlockRightJoint();
+
         for (int i = 0; i < jointCount; i++)
         {
             _lineRenderer.SetPosition(i, _joints[i].transform.position);
         }
+    }
+
+    private void LockLeftJoint()
+    {
+        ContactFilter2D filter = new ContactFilter2D();
+        LayerMask mask = new LayerMask();
+        mask.value = LayerMask.NameToLayer("Default");
+        filter.layerMask = mask;
+        Collider2D[] results = new Collider2D[10];
+        leftCollider.OverlapCollider(filter, results);
+
+        if (results[0] != null)
+        {
+            _joints.First().GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+    }
+
+    private void LockRightJoint()
+    {
+        ContactFilter2D filter = new ContactFilter2D();
+        LayerMask mask = new LayerMask();
+        mask.value = LayerMask.NameToLayer("Default");
+        filter.layerMask = mask;
+        Collider2D[] results = new Collider2D[10];
+        rightCollider.OverlapCollider(filter, results);
+
+        if (results[0] != null)
+        {
+            _joints.Last().GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+    }
+
+    private void UnlockLeftJoint()
+    {
+        _joints.First().GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+    }
+
+    private void UnlockRightJoint()
+    {
+        _joints.Last().GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
     }
 }
