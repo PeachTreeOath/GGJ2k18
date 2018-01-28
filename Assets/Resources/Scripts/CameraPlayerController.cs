02fec11fafd23fbb2b5df34fda500f4176df3102
PeachTreeOath <peachtreeoath@gmail.com>
2018-01-28 11:34:25 -0700

    Timer

--- Assets/Resources/Scripts/CameraPlayerController.cs
+++ Assets/Resources/Scripts/CameraPlayerController.cs
@@ -90,10 +90,12 @@ public class CameraPlayerController : MonoBehaviour
     {
         int minutes = Mathf.FloorToInt(timer / 60F);
         int seconds = Mathf.FloorToInt(timer - minutes * 60);
-        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
         GUIStyle style = new GUIStyle();
-        style.fontSize = 20;
-        
+        style.fontSize = 20;  
+        style.font = GucciGoldFont;
+        style.normal.textColor = new Color(0.15f, 0.15f, 0.15f);
+
 
          GUI.Label(new Rect(15, 10, 250, 100), niceTime, style);
     }
