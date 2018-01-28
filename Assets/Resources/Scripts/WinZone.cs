9a0130a21dda7cae4705288dd4d3ef51f6781ad7
PeachTreeOath <peachtreeoath@gmail.com>
2018-01-28 10:23:25 -0700

    Win zone change

--- Assets/Resources/Scripts/WinZone.cs
+++ Assets/Resources/Scripts/WinZone.cs
@@ -6,26 +6,6 @@ public class WinZone : MonoBehaviour {
 
     public SpriteRenderer sprite;
 
-    private bool canFade;
-    private Color alphaColor;
-    private float timeToFade = 1.0f;
-
-    public void Start()
-    {
-        canFade = false;
-        alphaColor = sprite.material.color;
-        alphaColor.a = 0;
-    }
-
-    public void Update()
-    {
-        Debug.Log(sprite.material.color.a);
-        if (canFade)
-        {
-            sprite.material.color = Color.Lerp(sprite.material.color, alphaColor, timeToFade * Time.deltaTime);
-        }
-    }
-
     void OnTriggerEnter2D(Collider2D col)
     {
         WinTrigger win = col.GetComponent<WinTrigger>();
@@ -40,6 +20,7 @@ public class WinZone : MonoBehaviour {
         PapermateBody paper = GameObject.Find("papermate").GetComponent<PapermateBody>();
         paper.FreezeBody();
 
-        canFade = true;
+        sprite.enabled = true;
+        sprite.transform.position = new Vector3(Camera.main.transform.position.x-3f, Camera.main.transform.position.y, sprite.transform.position.z);
     }
 }
