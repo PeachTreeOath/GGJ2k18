9f62c9a0bb8ee8a037ac997008ed98a1748dc709
PeachTreeOath <peachtreeoath@gmail.com>
2018-01-28 10:02:05 -0700

    Win zone stuff

--- Assets/Resources/Scripts/WinZone.cs
+++ Assets/Resources/Scripts/WinZone.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class WinZone : MonoBehaviour {
 
-	void OnTriggerEnter2D(Collider2D col)
+    public SpriteRenderer sprite;
+
+    private bool canFade;
+    private Color alphaColor;
+    private float timeToFade = 1.0f;
+
+    public void Start()
+    {
+        canFade = false;
+        alphaColor = sprite.material.color;
+        alphaColor.a = 0;
+    }
+
+    public void Update()
+    {
+        Debug.Log(sprite.material.color.a);
+        if (canFade)
+        {
+            sprite.material.color = Color.Lerp(sprite.material.color, alphaColor, timeToFade * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
     {
         WinTrigger win = col.GetComponent<WinTrigger>();
         if(win != null)
@@ -17,5 +39,7 @@ public class WinZone : MonoBehaviour {
     {
         PapermateBody paper = GameObject.Find("papermate").GetComponent<PapermateBody>();
         paper.FreezeBody();
+
+        canFade = true;
     }
 }
