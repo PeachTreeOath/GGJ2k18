1371ff89502c3f2d5092cbcbfcd724010ecffd58
Fangor <rfonger@gmail.com>
2018-02-06 21:17:45 -0700

    Starting some ps4 autodetect. This does not work yet

--- Assets/Resources/Scripts/InputManager.cs
+++ Assets/Resources/Scripts/InputManager.cs
@@ -12,6 +12,7 @@ class InputManager : Singleton<InputManager>
 {
     public Vector2 GetStick(Controls control)
     {
+
         switch (control)
         {
             case Controls.LeftStick:
@@ -32,5 +33,44 @@ class InputManager : Singleton<InputManager>
                 return Input.GetButtonDown("J_RightStickPress");
         }
         return false;
+    }
+
+    private int Xbox_One_Controller = 0;
+    private int PS4_Controller = 0;
+    void Update()
+    {
+        string[] names = Input.GetJoystickNames();
+        for (int x = 0; x < names.Length; x++)
+        {
+            print(names[x].Length);
+            if (names[x].Length == 19)
+            {
+                print("PS4 CONTROLLER IS CONNECTED");
+                PS4_Controller = 1;
+                Xbox_One_Controller = 0;
+            }
+            if (names[x].Length == 33)
+            {
+                print("XBOX ONE CONTROLLER IS CONNECTED");
+                //set a controller bool to true
+                PS4_Controller = 0;
+                Xbox_One_Controller = 1;
+
+            }
+        }
+
+
+        if (Xbox_One_Controller == 1)
+        {
+            //do something
+        }
+        else if (PS4_Controller == 1)
+        {
+            //do something
+        }
+        else
+        {
+            // there is no controllers
+        }
     }
 }
