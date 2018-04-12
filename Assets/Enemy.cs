87a880d4759f2c318cf3cb8a65f05effd65af988
Fangor <rfonger@gmail.com>
2018-04-12 11:53:04 -0600

    Started movement logic, not working yet

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -8,10 +8,33 @@ public class Enemy : MonoBehaviour {
     public float gravityValue;
     public Vector2 gravityDirection;
 
+    //For configuring movement
+    public float maxVelocity;
+    public float acceleration;
+    public bool isPatroling;
+
+    //For moving along a wall, positive is perpendicular moving right from the gravity vector, while negative is left.
+    private bool isMovingPositive = true;
+
     private void Update()
     {
         Rigidbody2D body = GetComponent<Rigidbody2D>();
         body.AddForce(gravityDirection * gravityValue);
+
+        //Now figure out movement 
+        if (isPatroling)
+        {
+            float movingPositiveSign = isMovingPositive ? 1 : -1;
+
+            print("QuaternionValue: " + Quaternion.Euler(0, movingPositiveSign * 90, 0));
+            Vector2 movementDirection = Quaternion.Euler(0, movingPositiveSign * 90, 0) * gravityDirection;
+            print("MovementDirection: " + movementDirection);
+            if (Mathf.Abs(body.velocity.magnitude) < maxVelocity)
+            {
+                body.AddForce(acceleration * movementDirection);
+                print("MovementForce: " + acceleration * movementDirection);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
