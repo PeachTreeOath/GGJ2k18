0b11a5b85931f3f909d1443e732ded4ada27ca8b
PeachTreeOath <peachtreeoath@gmail.com>
2018-04-15 12:45:59 -0600

    Cogs

---
+++ Assets/Resources/Scripts/Cog.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cog : MonoBehaviour
+{
+
+    public float rotateSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+}
